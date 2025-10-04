9421d66056a63eab167e7de2a24d4dbde691a4b6
Srikumar Subramanian <srikumar.subramanian@krea.edu.in>
2025-10-04 10:13:15 +0530

    Added continuous Level monitor (in addition to LED) and used Cairo API for rendering.

--- src/ui/ui.jl
+++ src/ui/ui.jl
@@ -2,6 +2,7 @@ module UI
 
 import Synth
 using Gtk4
+import Cairo
 using Printf
 
 RGBA = Gtk4.RGBA{Float32}
@@ -60,8 +61,10 @@ struct Level
     breadth::Int
     minval::Float32
     maxval::Float32
-    breakpts::Vector{Tuple{Float32,Float32}}
-    colours::Vector{Tuple{RGBA,RGBA}}
+    colours::Vector{Tuple{Float32, RGBA}}
+    canvas::Ref{Union{Nothing,GtkCanvas}}
+    valdisp::Ref{Union{Nothing,GtkLabel}}
+    value::Ref{Float32}
 end
 
 struct HGroup
@@ -160,7 +163,7 @@ function render(led :: LED, panel :: Panel)
     vertical = led.angle == 90
     led_width = horizontal ? led.length : led.breadth
     led_height = vertical ? led.length : led.breadth
-    box = GtkBox(vertical ? :v : h)
+    box = GtkBox(vertical ? :v : :h)
     label = GtkLabel(led.label)
     valdisp = GtkLabel("")
     canvas = GtkCanvas(led_width, led_height)
@@ -171,13 +174,13 @@ function render(led :: LED, panel :: Panel)
     led.valdisp[] = valdisp
     
     @guarded draw(canvas) do widget
+        ctx = Gtk4.getgc(widget)
+        w = Gtk4.width(widget)
+        h = Gtk4.height(widget)
         #println("In LED draw")
-        ctx = getgc(canvas)
-        h = height(canvas)
-        w = width(canvas)
-        rectangle(ctx, 0, 0, w, h)
-        set_source_rgb(ctx, 0, 0, 0)
-        fill(ctx)
+        Cairo.rectangle(ctx, 0, 0, w, h)
+        Cairo.set_source_rgb(ctx, 0, 0, 0)
+        Cairo.fill(ctx)
         v = led.value[]
         #println("Draw LED value ", v)
         ledw = w / led.numsteps
@@ -190,18 +193,77 @@ function render(led :: LED, panel :: Panel)
             x1 = vertical ? w - x0 : x0 + ledfillw
             y1 = vertical ? y0 + ledfillh : h - y0
             c = led.colours[i]
-            rectangle(ctx, x0, y0, x1-x0, y1-y0)
+            Cairo.rectangle(ctx, x0, y0, x1-x0, y1-y0)
             vmin = (i-1)/led.numsteps
             vmax = i/led.numsteps
             alpha = (max(vmin, min(vmax, v)) - vmin) / (vmax - vmin)
             gamma = 0.5
-            set_source_rgba(ctx, c.r, c.g, c.b, (alpha ^ gamma) * c.alpha)
-            fill(ctx)
+            Cairo.set_source_rgba(ctx, c.r, c.g, c.b, (alpha ^ gamma) * c.alpha)
+            Cairo.fill(ctx)
         end
     end
 
     panel.widgetof[led] = box
     return box
+end
+
+function render(level::Level, panel::Panel)
+    horizontal = level.angle == 0
+    vertical = level.angle == 90
+    level_width = horizontal ? level.length : level.breadth
+    level_height = vertical ? level.length : level.breadth
+    box = GtkBox(vertical ? :v : :h)
+    label = GtkLabel(level.label)
+    valdisp = GtkLabel("")
+    canvas = GtkCanvas(level_width, level_height)
+    push!(box, vertical ? valdisp : label)
+    push!(box, canvas)
+    push!(box, vertical ? label : valdisp)
+    level.canvas[] = canvas
+    level.valdisp[] = valdisp
+    
+    @guarded draw(canvas) do widget
+        ctx = Gtk4.getgc(widget)
+        w = Gtk4.width(widget)
+        h = Gtk4.height(widget)
+        Cairo.rectangle(ctx, 0, 0, w, h)
+        Cairo.set_source_rgb(ctx, 0, 0, 0)
+        fill(ctx)
+        v = level.value[]
+        gradient = if horizontal 
+            Cairo.pattern_create_linear(0, 0, w, 0) 
+        else
+            Cairo.pattern_create_linear(0, h, 0, 0)
+        end
+
+        for (stop, c) in level.colours
+            Cairo.pattern_add_color_stop_rgba(gradient, stop, c.r, c.g, c.b, c.alpha)
+        end
+
+        Cairo.set_source(ctx, gradient)
+        if horizontal
+            Cairo.rectangle(ctx, 0, 0, w * v, 0)
+        else
+            Cairo.rectangle(ctx, 0, (1.0f0-v) * h, w, h)
+        end
+        Cairo.fill(ctx)
+        Cairo.destroy(gradient)
+    end
+
+    panel.widgetof[level] = box
+    return box
+end
+
+function stdleds(ny::Integer, y::RGBA, no::Integer, o::RGBA, nr::Integer, r::RGBA)
+    vcat(repeat([y], ny), repeat([o], no), repeat([r], nr))
+end
+
+stdleds(y::RGBA,o::RGBA,r::RGBA) = stdleds(9, y, 3, o, 2, r)
+stdleds() = begin
+    y = RGBA(1.0f0, 1.0f0, 0.0f0, 1.0f0)
+    o = RGBA(1.0f0, 0.65f0, 0.0f0, 1.0f0)
+    r = RGBA(1.0f0, 0.0f0, 0.0f0, 1.0f0)
+    stdleds(y,o,r)
 end
 
 function led(label::String, angle::Int, length_::Int, breadth::Int, colours::Vector{RGBA}, source::Channel{Float32}; fillratio=0.5, gamma=0.5)
@@ -231,6 +293,40 @@ function led(label::String, angle::Int, length_::Int, breadth::Int, colours::Vec
     end
     return w
 end
+
+stdlevelcolours() = begin
+    g = RGBA(0.0f0, 1.0f0, 0.0f0, 1.0f0)
+    [(0.0f0, g), (1.0f0, g)]
+end
+
+function level(label::String, angle::Int, length_::Int, breadth::Int, colours::Vector{Tuple{Float32,RGBA}}, source::Channel{Float32})
+    @assert angle == 0 || angle == 90
+    N = length(colours)
+    val = Ref(0.0f0)
+    w = Level(label, angle, length_, breadth, 0.0f0, 1.0f0, colours, nothing, nothing, val)
+    @async begin
+        try
+            while true
+                v = take!(source)
+                val[] = v
+                #println("Level[$label] value ", v)
+                if !isnothing(w.valdisp[])
+                    #println("\t setting label")
+                    w.valdisp[].label = @sprintf("%0.2f", v)
+                end
+                if !isnothing(w.canvas[])
+                    #println("\t drawing canvas")
+                    draw(w.canvas[])
+                end
+            end
+        catch e
+            # Channel closed
+            nothing
+        end
+    end
+    return w
+end
+
 
 function render(k::Key, panel::Panel)
     btn = GtkButton(k.label * " (" * k.key * ")")
@@ -407,16 +503,16 @@ end
 src = Channel{Float32}(2)
 
 function test4()
-    y = RGBA(1.0f0, 1.0f0, 0.0f0, 1.0f0)
-    o = RGBA(1.0f0, 0.65f0, 0.0f0, 1.0f0)
-    r = RGBA(1.0f0, 0.0f0, 0.0f0, 1.0f0)
-    led("blinker", 90, 200, 40, vcat(repeat([y], 9), repeat([o],3), repeat([r], 2)), src)
+    led("blinker", 90, 200, 40, stdleds(), src)
+end
+function test5()
+    level("blinker", 90, 200, 40, stdlevelcolours(), src)
 end
 
 function test_main()
     app = GtkApplication("com.example.app", 0)
     signal_connect(app, :activate) do app
-        harnessed(test4, app)
+        harnessed(test5, app)
     end
     @async begin
         for i in 1:1000
