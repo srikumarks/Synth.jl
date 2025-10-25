7ea854a75c960b4dfb95fcfb4e781b29e034366a
Srikumar Subramanian <srikumar.subramanian@krea.edu.in>
2025-10-25 11:53:09 +0530

    Working Waveform UI. Tested independently. TO test in conjunction with probe.

--- src/ui/waveform.jl
+++ src/ui/waveform.jl
@@ -13,33 +13,40 @@ struct Waveform
 end
 
 function render(wf::Waveform, panel::Panel)
+    @debug "Rendering waveform box"
     box = GtkBox(:v)
-    label = GtkLabel(wg.label)
+    label = GtkLabel(wf.label)
     canvas = GtkCanvas(wf.width, wf.height)
     push!(box, canvas)
     push!(box, label)
-    wf.canvas = canvas
+    wf.canvas[] = canvas
 
-    maxs = zeros(Int, wf.width)
-    mins = zeros(Int, wf.width)
+    maxs = zeros(Float32, round(Int,wf.width))
+    mins = zeros(Float32, round(Int,wf.width))
+    @debug "Waveform box dimensions" width=wf.width height=wf.height
 
     @guarded draw(canvas) do widget
-        ctx = Gtk4.getgc(widget)
-        w = Gtk4.width(widget)
-        h = Gtk4.height(widget)
-        t = wf.val[1]
-        v = wf.val[2]
+        if isnothing(wf.val[])
+            return
+        end
 
-        label.label = @sprintf("[%s] %f : %f == %d : %d", wf.label, t[1], t[end], floor(Int, t[1] / t.step), ceil(Int, t[end]/t.step))
+        @debug "Drawing..."
+        ctx = Gtk4.getgc(widget)
+        w ::Int = round(Int, Gtk4.width(widget))
+        h ::Int = round(Int, Gtk4.height(widget))
+        t = wf.val[].span
+        v = wf.val[].samples
+
+        label.label = @sprintf("[%s] %f : %f == %d : %d", wf.label, t[1], t[end], floor(Int, t[1] / Float64(t.step)), ceil(Int, t[end]/Float64(t.step)))
         Cairo.rectangle(ctx, 0, 0, w, h)
-        Cairo.set_source_rgb(ctx, wf.bkgcolour.r, wf.blkcolour.g, wf.bkgcolour.b)
+        Cairo.set_source_rgb(ctx, wf.bkgcolour.r, wf.bkgcolour.g, wf.bkgcolour.b)
         Cairo.fill(ctx)
 
-       
+
         N = length(v)
         x0 = 0.0f0
         y0 = h/2.0f0
-        dy = h * 0.4
+        dy :: Float64 = h * 0.4
         if w != length(maxs)
             maxs = zeros(Float32,w)
             mins = zeros(Float32,w)
@@ -49,43 +56,43 @@ function render(wf::Waveform, panel::Panel)
                 i1 = div(i * N, w)
                 i2 = div((i+1)*N, w)
                 (mn,mx) = extrema(view(v, i1+1:i2))
-                maxs[i+1] = dy * mx
-                mins[i+1] = dy * mn
+                maxs[i+1] = Float32(dy * mx)
+                mins[i+1] = Float32(dy * mn)
             end
 
             # We treat the drawn waveform as a path that runs along all
             # the maxima and turns around and runs back along all the minima
             # and then we fill out the whole polygon.
-            Cairo.new_path(ctxt)
-            Cairo.move_to(ctxt, x0, y0 - maxs[1])
+            Cairo.new_path(ctx)
+            Cairo.move_to(ctx, x0, y0 - maxs[1])
             for i in 2:w
-                Cairo.line_to(ctxt, x0 + i-1, y0 - maxs[i])
+                Cairo.line_to(ctx, x0 + i-1, y0 - maxs[i])
             end
             for i in w:-1:1
-                Cairo.line_to(ctxt, x0 + i-1, y0 - mins[i])
+                Cairo.line_to(ctx, x0 + i-1, y0 - mins[i])
             end
-            Cairo.close_path(ctxt)
-            Cairo.set_source_rgb(ctxt, wf.wfcolour.r, wf.wfcolour.g, wf.wfcolour.b)
-            Cairo.stroke(ctxt)
-            Cairo.fill(ctxt)
+            Cairo.close_path(ctx)
+            Cairo.set_source_rgb(ctx, wf.wavecolour.r, wf.wavecolour.g, wf.wavecolour.b)
+            Cairo.stroke(ctx)
+            Cairo.fill(ctx)
         else
             # For other cases, we draw a linearly interpolated waveform.
-            Cairo.new_path(ctxt)
-            Cairo.move_to(ctxt, x0, y0 - dy * v[1])
-            for i in 2:w
-                Cairo.line_to(ctxt, x0 + i-1, y0 - dy * v[i])
+            Cairo.new_path(ctx)
+            Cairo.move_to(ctx, x0, y0 - dy * v[1])
+            for i in 2:length(v)
+                Cairo.line_to(ctx, x0 + i-1, y0 - dy * v[i])
             end
-            Cairo.set_source_rgb(ctxt, wf.wfcolour.r, wf.wfcolour.g, wf.wfcolour.b)
-            Cairo.stroke(ctxt)
+            Cairo.set_source_rgb(ctx, wf.wavecolour.r, wf.wavecolour.g, wf.wavecolour.b)
+            Cairo.stroke(ctx)
         end
 
         # Draw the clip bounds
-        Cairo.rectangle(ctxt, x0, y0 - dy - 1.0f0, w, 2.0f0)
-        Cairo.set_source_rgb(ctxt, 1.0f0, 0.0f0, 0.0f0)
-        Cairo.stroke(ctxt)
-        Cairo.rectangle(ctxt, x0, y0 + dy, w, 2.0f0)
-        Cairo.set_source_rgb(ctxt, 1.0f0, 0.0f0, 0.0f0)
-        Cairo.stroke(ctxt)
+        Cairo.rectangle(ctx, x0, y0 - dy - 1.0f0, w, 2.0f0)
+        Cairo.set_source_rgb(ctx, 1.0f0, 0.0f0, 0.0f0)
+        Cairo.stroke(ctx)
+        Cairo.rectangle(ctx, x0, y0 + dy, w, 2.0f0)
+        Cairo.set_source_rgb(ctx, 1.0f0, 0.0f0, 0.0f0)
+        Cairo.stroke(ctx)
     end
 
     panel.widgetof[wf] = box
@@ -94,13 +101,13 @@ end
 
 """
 """
-function waveform(
+function Waveform(
         label::String,
         width::Int,
         height::Int,
+        source::Channel{TimedSamples} = Channel{TimedSamples}(2);
         bkgcolour::RGBA = RGBA(0.0f0, 0.0f0, 0.0f0, 1.0f0),
-        wavecolour::RGBA = RGBA(0.4196f0, 0.7961f0, 0.4588f0, 1.0f0),
-        source::Channel{TimedSamples} = Channel{TimedSamples}(2)
+        wavecolour::RGBA = RGBA(0.4196f0, 0.7961f0, 0.4588f0, 1.0f0)
     )
     wf = Waveform(
                   label,
@@ -114,7 +121,8 @@ function waveform(
     @async begin
         try
             while true
-                val[] = take!(source)
+                wf.val[] = take!(source)
+                @debug "TimedSamples received by waveform box" len=(length(wf.val[].span), length(wf.val[].samples))
                 if !isnothing(wf.canvas[])
                     draw(wf.canvas[])
                 end
