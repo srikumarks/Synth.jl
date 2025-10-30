218dfebd246de693cd8a9df584581bff943ca613
Srikumar Subramanian <srikumar.subramanian@krea.edu.in>
2025-10-30 08:07:52 +0530

    Added doc strings for drumkit and hit functions.

--- src/models/drumkit.jl
+++ src/models/drumkit.jl
@@ -1,5 +1,13 @@
 using ..Synth: Gen, Bus, sched, Cont
 
+"""
+Represents a sampled drum sound to be played without
+a "duration". You can use the [`hit`])(@ref) function
+with it to reuse it with a different velocity.
+
+- `samplefile` - file name of the sample containing the drum hit.
+- `vel` - the velocity of the hit - in the range [0.0,1.0].
+"""
 struct DrumHit <: Gen
     samplefile::String
     vel::Float32
@@ -10,16 +18,34 @@ function Synth.proc(g::DrumHit, b::Bus, t)
     (t, Cont())
 end
 
-function hit(samplefile::AbstractString, vel::Float32 = 1.0f0)
+"""
+    hit(samplefile::AbstractString, vel::Real = 1.0f0) :: DrumHit
+    hit(drum::DrumHit, vel::Real = 1.0f0) :: DrumHit
+
+Constructs a [`DrumHit`](@ref) that can be used in `Gen` compositions.
+Drum hits have an inherent duration of 0.0. So if you want to put a 
+gap between two drum hits, you'll need to use [`pause`](@ref).
+`hit` will load the sample and cache it so the loading doesn't
+happen at actual play time.
+
+Usage: `play(track([hit("kick.wav", 0.5), pause(0.5), hit("snare.wav", 0.5)]), 2.0)`
+
+See also [`Synth.Models.drumkit`](@ref)
+"""
+function hit(samplefile::AbstractString, vel::Real = 1.0f0)
     @assert isfile(samplefile) "Sample file '$samplefile' not found"
     sample(samplefile)
-    DrumHit(samplefile, vel)
+    DrumHit(samplefile, Float32(vel))
 end
 
 function hit(drum::DrumHit, vel::Float32 = 1.0f0)
     DrumHit(drum.samplefile, vel)
 end
 
+"""
+Collects together a number of "standard" drums belonging to
+a "kit".
+"""
 struct DrumKit
     kick::DrumHit
     hihat::DrumHit
@@ -29,21 +55,36 @@ struct DrumKit
     tom3::DrumHit
 end
 
-function drumkit(kit::AbstractString; dir::Union{Nothing,AbstractString} = nothing)
-    modelsdir = @__DIR__
-    kitspath = if isnothing(dir) "$modelsdir/drumkits" else dir end
+"""
+    drumkit(kit::AbstractString, dir::AbstractString) :: DrumKit
+    drumkit(kit::AbstractString) :: DrumKit
 
+Loads a "standard" drum kit consisting of a kick, hihat, snare and three toms.
+`kit` gives the name of the kit to load and maps to the name of a directory
+under the given `dir`. If `dir` is omitted, then the kits included within
+Synth.jl are scanned.
+
+Usage:
+
+```
+using Synth.Models: drumkit
+
+k = drumkit("Techno")
+play(track([hit(k.kick, 0.5), pause(0.5), hit(k.snare, 0.5)]), 2.0)
+```
+
+See also [`Synth.Models.hit`](@ref)
+"""
+function drumkit(kit::AbstractString, dir::AbstractString)
+    @assert isdir(dir) "Invalid drumkit directory $dir"
+    @assert isdir("$dir/$kit") "Invalid drumkit $kit within $dir"
     DrumKit(
-            hit("$kitspath/$kit/kick.wav"),
-            hit("$kitspath/$kit/snare.wav"),
-            hit("$kitspath/$kit/hihat.wav"),
-            hit("$kitspath/$kit/tom1.wav"),
-            hit("$kitspath/$kit/tom2.wav"),
-            hit("$kitspath/$kit/tom3.wav")
+            hit("$dir/$kit/kick.wav"),
+            hit("$dir/$kit/snare.wav"),
+            hit("$dir/$kit/hihat.wav"),
+            hit("$dir/$kit/tom1.wav"),
+            hit("$dir/$kit/tom2.wav"),
+            hit("$dir/$kit/tom3.wav")
            )
 end
-
-function drumkits()
-    modelsdir = @__DIR__
-    [d for d in readdir("$modelsdir/drumkits/") if isdir("$modelsdir/drumkits/$d")]
-end
+drumkit(kit::AbstractString) = drumkit(kit, (@__DIR__) * "/drumkits")
